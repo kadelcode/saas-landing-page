3c0debdfc252d34cfe013c9ee0b0d5d06a123b4a
kadelcode <kadelcode@gmail.com>
2025-03-08 09:42:53 +0100

    Revert code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-500 bg-gray-100 bg-gray-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         {children}
       </body>
