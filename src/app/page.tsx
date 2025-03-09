ba9a802a1345bfdbda33818c2c4fd684bead77ad
kadelcode <kadelcode@gmail.com>
2025-03-09 10:02:45 +0100

    Refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import { ThemeProvider } from "next-themes";
 import { Hero } from "@/components/sections/hero";
 import { Features } from "@/components/sections/features";
 import { Pricing } from "@/components/sections/pricing";
 
 export default function Home() {
   return (
-    <ThemeProvider attribute="class">
-      <div className="min-h-screen bg-background text-foreground">
-        {/* Hero Section */}
-        <Hero />
-        <Features />
-        <Pricing />
-      </div>
-    </ThemeProvider>
+    <div className="min-h-screen bg-background text-foreground">
+      {/* Hero Section */}
+      <Hero />
+      <Features />
+      <Pricing />
+    </div>
   )
 }
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,38 +1,16 @@
 "use client";
-
-import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Sun, Moon} from 'lucide-react';
-import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import ThemeToggle from '@/components/ThemeToggle';
 
 
 export function Hero() {
-    const { theme, setTheme } = useTheme();
-    const [ mounted, setMounted ] = useState(false);
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
 
     return (
         <section className='relative flex flex-col items-center justify-center text-center py-20 px-6'>
-            {/* Dark Mode Toggle */}
-            {mounted && (
-                <div className='fixed top-6 right-6'>
-                    <button
-                      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                      className='p-2 rounded-full bg-gray-200 dark:bg-gray-800 hover:cursor-pointer'
-                    >
-                        {theme === 'dark' ?
-                        <Sun className='w-6 h-6' /> :
-                        <Moon className='w-6 h-6' />
-                        }
-                    </button>
-                </div>
-            )}
-
+            <ThemeToggle />
             {/* Hero Content */}
             <motion.h1
               initial={{ opacity: 0, y: -20 }}
