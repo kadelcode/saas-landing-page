2b945871154de7c5b36965e84a182defa142a2d5
kadelcode <kadelcode@gmail.com>
2025-03-09 23:41:01 +0100

    Remove Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Features } from "@/components/sections/features";
 import { Pricing } from "@/components/sections/pricing";
 import { Testimonials } from "@/components/sections/Testimonials";
 import { FAQ } from "@/components/sections/faq";
-import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -14,7 +13,6 @@ export default function Home() {
       <Pricing />
       <Testimonials />
       <FAQ />
-      <Footer />
     </div>
   )
 }
